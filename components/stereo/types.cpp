e61d6204103b423e906872d45f6e64b75a5bc40b
Mads Buvik Sandvei <madssandvei@protonmail.com>
2025-01-21 19:21:34 +0100

    Clang'd

--- components/stereo/types.cpp
+++ components/stereo/types.cpp
@@ -176,8 +176,8 @@ namespace Stereo
         double angle_x = 0.0;
         double angle_y = 0.0;
         double angle_z = 0.0;
-        double D, C, tr_x, tr_y;
-        angle_y = D = asin(mat[2]); /* Calculate Y-axis angle */
+        double C, tr_x, tr_y;
+        angle_y = asin(mat[2]); /* Calculate Y-axis angle */
         C = cos(angle_y);
         if (fabs(C) > 0.005) /* Test for Gimball lock? */
         {
